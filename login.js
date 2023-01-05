1b358d3e7c1b92706eca751b077452d17111dd2c
dheeraj303 <singh.dheeraj6303@gmail.com>
2023-01-05 21:54:15 +0530

    login backend added

---
+++ login.js
@@ -0,0 +1,28 @@
+
+const email=document.querySelector('#email');
+const password=document.querySelector('#password');
+
+document.getElementById('login').addEventListener('click',signup);
+
+async function signup(e){
+    e.preventDefault();
+    const data={
+       
+        "email":email.value,
+        "password":password.value
+    }
+    try{
+        let response=await axios.post('http://localhost:3000/login',data)
+        console.log(response.data[0]);
+     if(response.data[0].status==1){
+        document.getElementById('msg').textContent=response.data[0].message;
+        // window.SharedArrayBuffer.location
+     }
+     if(response.data[0].status==0){
+        document.getElementById('msg').textContent=response.data[0].message;
+     }
+    }
+    catch(err){
+        console.log(err);
+    }
+}
