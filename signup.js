72cf714371f3fc9b5fb9cab609d82ffb4e37a657
dheeraj303 <singh.dheeraj6303@gmail.com>
2023-01-05 15:02:22 +0530

    attatch signup api

--- signup.js
+++ signup.js
@@ -1,6 +1,6 @@
 const name=document.querySelector('#name');
 const email=document.querySelector('#email');
-const phone=document.querySelector('#phone');
+const password=document.querySelector('#password');
 
 document.getElementById('signup').addEventListener('click',signup);
 
@@ -9,8 +9,20 @@ async function signup(e){
     const data={
         "name":name.value,
         "email":email.value,
-        "phone":phone.value
+        "password":password.value
     }
-
-    axios.post('')
+    try{
+        let response=await axios.post('http://localhost:3000/signup',data)
+        console.log(response.data[0]);
+     if(response.data[0].status==1){
+        document.getElementById('msg').textContent=response.data[0].message;
+        // window.SharedArrayBuffer.location
+     }
+     if(response.data[0].status==0){
+        document.getElementById('msg').textContent=response.data[0].message.name;
+     }
+    }
+    catch(err){
+        console.log(err);
+    }
 }
