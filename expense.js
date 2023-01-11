96c2f840096674833c082aa14a929b2880c213cf
dheeraj303 <singh.dheeraj6303@gmail.com>
2023-01-11 23:25:37 +0530

    payment gateway front

--- expense.js
+++ expense.js
@@ -141,3 +141,30 @@ function deletefromlist(amount){
         parentnode.removeChild(nodedelete);
     }
    }
+
+
+   document.getElementById('rzp-button1').onclick=async function(e){
+    const token=localStorage.getItem('token');
+    let response=await axios.get('http://localhost:3000/purchase/premiummembership',{headers:{"Authorization":token}});
+    console.log(response);
+    var options={
+        "key":response.data.key_id,
+        "order_id":response.data.order.id,
+        "handler":async function(response){
+            await axios.post('http://localhost:3000/purchase/updatetransactionstatus',{
+                order_id:options.order_id,
+                payment_id:response.razorpay_payment_id
+            },{headers:{"Authorization":token}})
+            alert("You are a Premium User");
+        }
+    }
+    var rzp1 = new Razorpay(options);
+ 
+      rzp1.open();
+      e.preventDefault();
+    rzp1.on('payment.failed',function(response){
+        console.log(response);
+        alert('Something Went Wrong');
+    })
+   }
+  
